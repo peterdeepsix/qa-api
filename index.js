2cd92919719c336b120598776e8c78f11d918413
Peter Arnold <peter@deepsixdesign.com>
2020-05-29 17:27:06 -0400

    asd

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const { QAClient } = require("question-answering");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const dateStart = Date.now();
-
 const text = `
   Super Bowl 50 was an American football game to determine the champion of the National Football League (NFL) for the 2015 season.
   The American Football Conference (AFC) champion Denver Broncos defeated the National Football Conference (NFC) champion Carolina Panthers 24–10 to earn their third Super Bowl title. The game was played on February 7, 2016, at Levi's Stadium in the San Francisco Bay Area at Santa Clara, California.
@@ -18,13 +16,14 @@ const predict = async () => {
     const qaClient = await QAClient.fromOptions();
     const answer = await qaClient.predict(question, text);
     console.log(answer);
+    return answer
+}
 
 app.get('/', (req, res) => {
-  const today = new Date();
-    predict()
+  const answer = predict()
   res.json({
-    date: today,
-    up: `${(Date.now() - dateStart) / 1000} seg.` 
+    answer: answer.text,
+    score: answer.score
   });
 });
 
