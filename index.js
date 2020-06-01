bf15cd442b28fac66cb0383372828de636d79b57
Peter Arnold <peter@deepsixdesign.com>
2020-06-01 13:26:15 -0400

    asd

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.post('/questions', async (req, res) => {
-  const input = req.body
+  const input = {
+    "text": "Super Bowl 50 was an American football game to determine the champion of the National Football League (NFL) for the 2015 season.\n    The American Football Conference (AFC) champion Denver Broncos defeated the National Football Conference (NFC) champion Carolina Panthers 24–10 to earn their third Super Bowl title. The game was played on February 7, 2016, at Levi's Stadium in the San Francisco Bay Area at Santa Clara, California.\n    As this was the 50th Super Bowl, the league emphasized the \"golden anniversary\" with various gold-themed initiatives, as well as temporarily suspending the tradition of naming each Super Bowl game with Roman numerals (under which the game would have been known as \"Super Bowl L\"), so that the logo could prominently feature the Arabic numerals 50.",
+    "question": "Who won the super bowl?"
+}
   const model = await initModel({
     name: "distilbert-base-cased-distilled-squad",
     path: "https://qa-serving-f6hsrmjybq-uc.a.run.app/v1/models/cased",
     runtime: "remote"
   });
-  
+
   const qaClient = await QAClient.fromOptions({ model });
   const answer = await qaClient.predict(input.question, input.text);
 
