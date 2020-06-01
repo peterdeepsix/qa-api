d8628bb6a8ce0fa7d0a52ca9d03d8567275326c9
Peter Arnold <peter@deepsixdesign.com>
2020-06-01 13:11:48 -0400

    asd

--- index.js
+++ index.js
@@ -4,15 +4,9 @@ const { initModel, QAClient } = require("question-answering");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', async (req, res) => {
-  const text = `
-    Super Bowl 50 was an American football game to determine the champion of the National Football League (NFL) for the 2015 season.
-    The American Football Conference (AFC) champion Denver Broncos defeated the National Football Conference (NFC) champion Carolina Panthers 24–10 to earn their third Super Bowl title. The game was played on February 7, 2016, at Levi's Stadium in the San Francisco Bay Area at Santa Clara, California.
-    As this was the 50th Super Bowl, the league emphasized the "golden anniversary" with various gold-themed initiatives, as well as temporarily suspending the tradition of naming each Super Bowl game with Roman numerals (under which the game would have been known as "Super Bowl L"), so that the logo could prominently feature the Arabic numerals 50.
-  `;
-
-  const question = "Who won the Super Bowl?";
-
+app.post('/questions', async (req, res) => {
+  const text = req.body.text
+  const question = req.body.question
   const model = await initModel({
     name: "distilbert-base-cased-distilled-squad",
     path: "https://qa-serving-f6hsrmjybq-uc.a.run.app/v1/models/cased",
